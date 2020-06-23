b0798f41f4eb4e815aee2d8236d5728f537bbbaa
Olga Gladiy <bel-olga@yandex.ru>
2020-06-23 20:54:27 +0300

    Added name to print

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ public class Main {
         BmiService service = new BmiService();
 
         float bmi = service.calculate (50,170);
-        System.out.println(bmi);
+        System.out.printf("Индекс Массы Тела %.1f ",bmi);
+
+        float bmi1 = service.calculate (100,180);
+        System.out.printf("Индекс Массы Тела %.1f ",bmi1);
 
 
-        float bmi1 = service.calculate(100,180);
-        System.out.println(bmi1);
         }
 }
